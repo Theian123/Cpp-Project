33eddf8a28e2c13f0b66cd32202dd75a70e64e79
The Ian <88885586+wangyichen20100106@users.noreply.github.com>
2023-02-11 23:49:31 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,12 +1,27 @@
-﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
+#include<Windows.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!"<<endl;
+    double a;
+    cout << "输入循环次数" << endl;
+    cin>>a;
+    if (a > 0)
+    {
+        for (double i = 1; i <= a; i++)
+            cout<< a <<endl;
+    }
+    if (a <= 0)
+    {
+        while (a <= 0)
+        {
+            cout << a << endl;
+        }
+    }
     return 0;
 }
 
